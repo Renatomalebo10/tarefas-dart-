e6ddca3d2676e97ccb55b265836d68dd64665524
Renato Malebo <renatomalebo10@gmail.com>
2026-05-21 19:50:02 +0100

    feat: Adicionado a opção adicionar.

---
+++ SistemaGestãoTarefa/SistemaGestãoTarefa.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main(){
+
+   List<String> tarefas = [];
+
+     while (true) {
+          print("+++++++++++ Bem Vindo ++++++++++");
+          print("1 - Adicionar");
+          print("2 - Ver");
+          print("3 - Editar");
+          print("4 - Remover");
+          print("0 - Sair");
+          int opcao = int.parse(stdin.readLineSync()!);
+
+          switch(opcao){
+            case 1:
+              print("Digite a tarefa : ");
+              String tarefa = stdin.readLineSync()!;
+              tarefas.add(tarefa);
+              break;
+              
+          }
+     }
+
+}
