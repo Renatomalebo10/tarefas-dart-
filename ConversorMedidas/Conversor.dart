442e0cf7f5c176a0fe6130535961bcb392db8029
Renato Malebo <renatomalebo10@gmail.com>
2026-05-22 21:32:40 +0100

    feat: Primeiro menu feito, ela tem a função de converter de kilometro para metros, pegando o valor digitado pelo o usuario e multiplicando por 1000.

---
+++ ConversorMedidas/Conversor.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main(){
+  int opcao = 0;
+
+   do {
+    print("+++++++++ Conversor +++++++++++");
+    print("1 - Km para Metro");
+    print("2 - Metro para Centrimetro");
+    print("3 - Celsius para Fahrenheit");
+    print("4 - Kg para Gramas");
+    print("++++++++++++++++++++++++++++++++");
+    opcao = int.parse(stdin.readLineSync()!);
+
+    switch(opcao){
+      case 1:
+            print("===========================");
+            print("Insere o valor em Km");
+            double km = double.parse(stdin.readLineSync()!);
+            double kmParaMetro(double Km){
+                return Km * 1000;
+            }
+            print("${kmParaMetro(km)} metros");
+            print("============================");
+            break;
+      
+      
+
+    } 
+     
+   } while (opcao != 0);
+}
+
+
+
+
